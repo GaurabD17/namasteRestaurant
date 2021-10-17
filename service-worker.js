96279982c30f3919013c00f7bf7693307fdf1547
GaurabD17 <gaurabdhakal77@gmail.com>
2021-10-17 19:12:36 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/namasteRestaurant/precache-manifest.84dab05d457a747c72cfde15dcc47efa.js"
+  "/namasteRestaurant/precache-manifest.ac0402dea61ab4f2cefe38ab0c57689c.js"
 );
 
 self.addEventListener('message', (event) => {
